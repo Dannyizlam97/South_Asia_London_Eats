9031c4c8c544975d6dc3a38a011cdab18db44dcf
Dannyizlam97 <daniyalmislam@gmail.com>
2026-07-03 17:32:43 +0200

    Add South Asian London restaurant directory (static site)

---
+++ data.js
@@ -0,0 +1,34 @@
+// Restaurant data. Replace these placeholder entries with real data.
+// Fields:
+//   name    - restaurant name
+//   area    - London area / neighbourhood
+//   cuisine - e.g. "Indian", "Pakistani", "Bangladeshi", "Sri Lankan", "Nepalese"
+//   price   - one of "£", "££", "£££"
+//   note    - a short description
+//   url     - website link (https://...)
+const RESTAURANTS = [
+  {
+    name: "Tayyabs",
+    area: "Whitechapel",
+    cuisine: "Pakistani",
+    price: "££",
+    note: "Lamb chops are the best",
+    url: "https://www.tayyabs.co.uk/",
+  },
+  {
+    name: "Lahore Karahi",
+    area: "Southall",
+    cuisine: "Punjabi",
+    price: "£",
+    note: "Karahi chicken is the best",
+    url: "https://www.lahorekarahi.co.uk/",
+  },
+  {
+    name: "Dawat",
+    area: "Tooting",
+    cuisine: "Punjabi",
+    price: "££",
+    note: "Seekh kebabs are delicious",
+    url:"https://dawatgroup.co.uk/",
+  },
+];
